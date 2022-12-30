8d04cc9e3df4074fbd2475099b1c5bc3399b143e
hiromikoreyes <114784445+hiromikoreyes@users.noreply.github.com>
2022-12-30 17:05:21 -0500

    added display functions

--- app.js
+++ app.js
@@ -30,3 +30,37 @@ function operate(operator, a, b){
             return divide(a,b);
     }
 }
+
+/* 
+Display Functions
+*/
+
+const displayScreen = document.querySelector('#display-screen');
+
+displayValue = '';
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+const zero = document.querySelector('#zero');
+
+one.addEventListener('click', displayNumber('1'));
+two.addEventListener('click', displayNumber('2'));
+three.addEventListener('click', displayNumber('3'));
+four.addEventListener('click', displayNumber('4'));
+five.addEventListener('click', displayNumber('5'));
+six.addEventListener('click', displayNumber('6'));
+seven.addEventListener('click', displayNumber('7'));
+eight.addEventListener('click', displayNumber('8'));
+nine.addEventListener('click', displayNumber('9'));
+zero.addEventListener('click', displayNumber('0'));
+
+function displayNumber(num){
+    displayValue += num;
+    displayScreen.textContent = displayValue;
+}
